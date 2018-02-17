85c9d1ce0982ee5a35a7975def9390ab3f953b88
Oliver Smith <ollieparanoid@bitmessage.ch>
2018-02-17 21:43:35 +0000

    Formatting and some fixes
    Formatting:
    * Break long lines
    * Add line at end of file
    * Begin comment lines with `#<space>` instead of `#<tab>`
    * `func()` instead of `func ()` to match APKBUILD style, e.g.:
    https://wiki.alpinelinux.org/wiki/APKBUILD_examples:Simple
    
    Fixes:
    * Make check for pmbootstrap.py simpler
    * kconfig_check: provide linux packages (kconfig: kernel config)
    * aportgen: only works with few aports, so removed it from the list
    * pkgrel_bump: works with all packages, added
    
    Other:
    * Reword "incomplete" notice above, this works quite well now :)

--- helpers/pmbootstrap-autocompletion.zsh
+++ helpers/pmbootstrap-autocompletion.zsh
@@ -1,38 +1,39 @@
 #!zsh
-#	Installation: 
+# Installation:
 #
-#	Copy this file to ~/.zsh/ (create it, if it doesn't exist, or put it 
-#	somewhere that makes sense to you) Then, insert the following line 
-#	in your ~/.zshrc (making sure to use the right folder name, if changed):
+# Copy this file to ~/.zsh/ (create it, if it doesn't exist, or put it
+# somewhere that makes sense to you). Then, insert the following line
+# in your ~/.zshrc (making sure to use the right folder name, if changed):
 #
-#		source ~/.zsh/pmbootstrap-auto-completion.zsh
+#	source ~/.zsh/pmbootstrap-auto-completion.zsh
 #
-#	Then, set the variable PMBOOTSTRAP_DIR to your `pmbootstrap` root.
-#	Example:
+# Then, set the variable PMBOOTSTRAP_DIR to your `pmbootstrap` root.
+# Example:
 #
-#		PMBOOTSTRAP_DIR=/home/axel/Git/pmbootstrap
-#	
-#	This file is VERY incomplete, and should really only be trusted to
-#	autocomplete directory names at this time.
+#	PMBOOTSTRAP_DIR=/home/axel/Git/pmbootstrap
+#
+# This file is rudimentary, pmbootstrap actions and packages are autocompleted
+# so far. Further ideas for improvements are here:
+# <https://github.com/postmarketOS/pmbootstrap/pull/1232>
 
 PMBOOTSTRAP_DIR=
 
-_pmbootstrap_commands () 
+_pmbootstrap_commands()
 {
-	grep '^def ' $PMBOOTSTRAP_DIR/pmb/helpers/frontend.py | cut -d ' ' -f 2 | cut -d '(' -f 1 | grep -v '^_'
+	grep '^def ' $PMBOOTSTRAP_DIR/pmb/helpers/frontend.py | cut -d ' ' -f 2 \
+		| cut -d '(' -f 1 | grep -v '^_'
 }
 
-_pmbootstrap_targets ()
+_pmbootstrap_targets()
 {
 	case $1 in
-		checksum|aportgen|build)
-			find $PMBOOTSTRAP_DIR/aports/ -mindepth 2 -type d -printf  '%f\n' | sed "s|$PMBOOTSTRAP_DIR/aports/||g"
+		build|checksum|pkgrel_bump)
+			find $PMBOOTSTRAP_DIR/aports/ -mindepth 2 -maxdepth 2 -type d \
+				-printf '%f\n' | sed "s|$PMBOOTSTRAP_DIR/aports/||g"
 			;;
-		kconfig_check)
-			ls -1 $PMBOOTSTRAP_DIR/aports/device/
-			;;
-		menuconfig)
-			ls -1 $PMBOOTSTRAP_DIR/aports/device/ | grep linux- | sed 's/linux-//g'
+		kconfig_check|menuconfig)
+			ls -1 $PMBOOTSTRAP_DIR/aports/*/ | grep linux- \
+				| sed 's/linux-//g'
 			;;
 		flasher)
 			echo flash_kernel flash_system
@@ -40,7 +41,7 @@ _pmbootstrap_targets ()
 	esac
 }
 
-_pmbootstrap ()
+_pmbootstrap()
 {
 	local curcontext="$curcontext" state line
 	typeset -A opt_args
@@ -48,7 +49,7 @@ _pmbootstrap ()
 	_arguments -C \
 		'1: :->command'\
 		'2: :->target'
-		
+
 		case $state in
 			command)
 				compadd `_pmbootstrap_commands`
@@ -59,6 +60,6 @@ _pmbootstrap ()
 		esac
 }
 
-if [ -d "$PMBOOTSTRAP_DIR" ] && [ -f $PMBOOTSTRAP_DIR/pmbootstrap.py ]; then
+if [ -f $PMBOOTSTRAP_DIR/pmbootstrap.py ]; then
 	compdef _pmbootstrap pmbootstrap
 fi
